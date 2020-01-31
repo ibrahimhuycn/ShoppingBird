b911d5e537de41f9109625ea090058d514f21c22
Ibrahim Hussain <ibrahim.hucyn@live.com>
2020-01-31 18:43:05 +0500

    Initial Setup, Web UI

---
+++ ShoppingBirdWeb/Data/Product.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShoppingBirdWeb.Data
+{
+    public class Product
+    {
+        public string Barcode { get; set; }
+        public string Description { get; set; }
+        public float RetailPrice { get; set; }
+
+    }
+}
