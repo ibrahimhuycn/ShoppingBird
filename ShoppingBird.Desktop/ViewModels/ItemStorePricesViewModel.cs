7ee0bcf4c7320ca89396ab71cf9486eab821e3ab
Ibrahim Hussain <ibrahim.hucyn@live.com>
2022-10-28 13:44:58 +0500

    ItemStorePrices ViewModel

---
+++ ShoppingBird.Desktop/ViewModels/ItemStorePricesViewModel.cs
@@ -0,0 +1,31 @@
+﻿using ShoppingBird.Desktop.Models;
+using ShoppingBird.Fly.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShoppingBird.Desktop.ViewModels
+{
+    public class ItemStorePricesViewModel
+    {
+        private readonly IStoreIO _storeIO;
+        private readonly IUnitsIO _unitsIO;
+        private readonly IItemIO _itemIO;
+
+        public ItemStorePricesViewModel(IStoreIO storeIO, IUnitsIO unitsIO, IItemIO itemIO)
+        {
+            _storeIO = storeIO;
+            _unitsIO = unitsIO;
+            _itemIO = itemIO;
+
+            AllStores = new BindingList<StoreModel>();
+            AllUnits =new BindingList<UnitsModel>();
+        }
+
+        public BindingList<StoreModel> AllStores { get; set; }
+        public BindingList<UnitsModel> AllUnits { get; set; }
+    }
+}
