675da1f858b957701f008aa4d4e50aca9cf70876
Ibrahim Hussain <ibrahim.hucyn@live.com>
2022-10-28 14:05:25 +0500

    Added required layouts to ItemStorePriceView

--- ShoppingBird.Desktop/Views/ItemStorePricesView.Designer.cs
+++ ShoppingBird.Desktop/Views/ItemStorePricesView.Designer.cs
@@ -30,20 +30,197 @@ namespace ShoppingBird.Desktop.Views
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ItemStorePricesView));
+            this.splitContainerMain = new System.Windows.Forms.SplitContainer();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.groupControlStore = new DevExpress.XtraEditors.GroupControl();
+            this.groupControlUnits = new DevExpress.XtraEditors.GroupControl();
+            this.splitContainerPrices = new System.Windows.Forms.SplitContainer();
+            this.splitContainer2 = new System.Windows.Forms.SplitContainer();
+            this.groupControlItems = new DevExpress.XtraEditors.GroupControl();
+            this.groupControlAddEditPrices = new DevExpress.XtraEditors.GroupControl();
+            this.groupControlCurrentPrices = new DevExpress.XtraEditors.GroupControl();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainerMain)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainerMain.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.groupControlStore)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.groupControlUnits)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainerPrices)).BeginInit();
+            this.splitContainer2.Panel1.SuspendLayout();
+            this.splitContainer2.Panel2.SuspendLayout();
+            this.splitContainerPrices.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).BeginInit();
+            this.splitContainer2.Panel1.SuspendLayout();
+            this.splitContainer2.Panel2.SuspendLayout();
+            this.splitContainer2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.groupControlItems)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.groupControlAddEditPrices)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.groupControlCurrentPrices)).BeginInit();
             this.SuspendLayout();
+            // 
+            // splitContainerMain
+            // 
+            this.splitContainerMain.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainerMain.Location = new System.Drawing.Point(0, 0);
+            this.splitContainerMain.Name = "splitContainerMain";
+            // 
+            // splitContainerMain.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.splitContainer1);
+            // 
+            // splitContainerMain.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.splitContainerPrices);
+            this.splitContainerMain.Size = new System.Drawing.Size(1436, 702);
+            this.splitContainerMain.SplitterDistance = 355;
+            this.splitContainerMain.TabIndex = 0;
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.groupControlStore);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.groupControlUnits);
+            this.splitContainer1.Size = new System.Drawing.Size(355, 702);
+            this.splitContainer1.SplitterDistance = 334;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // groupControlStore
+            // 
+            this.groupControlStore.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupControlStore.Location = new System.Drawing.Point(0, 0);
+            this.groupControlStore.Name = "groupControlStore";
+            this.groupControlStore.Size = new System.Drawing.Size(355, 334);
+            this.groupControlStore.TabIndex = 0;
+            this.groupControlStore.Text = "ALL STORES";
+            // 
+            // groupControlUnits
+            // 
+            this.groupControlUnits.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupControlUnits.Location = new System.Drawing.Point(0, 0);
+            this.groupControlUnits.Name = "groupControlUnits";
+            this.groupControlUnits.Size = new System.Drawing.Size(355, 364);
+            this.groupControlUnits.TabIndex = 1;
+            this.groupControlUnits.Text = "ALL UNITS";
+            // 
+            // splitContainerPrices
+            // 
+            this.splitContainerPrices.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainerPrices.Location = new System.Drawing.Point(0, 0);
+            this.splitContainerPrices.Name = "splitContainerPrices";
+            // 
+            // splitContainerPrices.Panel1
+            // 
+            this.splitContainer2.Panel1.Controls.Add(this.splitContainer2);
+            // 
+            // splitContainerPrices.Panel2
+            // 
+            this.splitContainer2.Panel2.Controls.Add(this.groupControlCurrentPrices);
+            this.splitContainerPrices.Size = new System.Drawing.Size(1077, 702);
+            this.splitContainerPrices.SplitterDistance = 359;
+            this.splitContainerPrices.TabIndex = 0;
+            // 
+            // splitContainer2
+            // 
+            this.splitContainer2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer2.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer2.Name = "splitContainer2";
+            this.splitContainer2.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer2.Panel1
+            // 
+            this.splitContainer2.Panel1.Controls.Add(this.groupControlItems);
+            // 
+            // splitContainer2.Panel2
+            // 
+            this.splitContainer2.Panel2.Controls.Add(this.groupControlAddEditPrices);
+            this.splitContainer2.Size = new System.Drawing.Size(359, 702);
+            this.splitContainer2.SplitterDistance = 335;
+            this.splitContainer2.TabIndex = 0;
+            // 
+            // groupControlItems
+            // 
+            this.groupControlItems.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupControlItems.Location = new System.Drawing.Point(0, 0);
+            this.groupControlItems.Name = "groupControlItems";
+            this.groupControlItems.Size = new System.Drawing.Size(359, 335);
+            this.groupControlItems.TabIndex = 1;
+            this.groupControlItems.Text = "ALL ITEMS";
+            // 
+            // groupControlAddEditPrices
+            // 
+            this.groupControlAddEditPrices.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupControlAddEditPrices.Location = new System.Drawing.Point(0, 0);
+            this.groupControlAddEditPrices.Name = "groupControlAddEditPrices";
+            this.groupControlAddEditPrices.Size = new System.Drawing.Size(359, 363);
+            this.groupControlAddEditPrices.TabIndex = 2;
+            this.groupControlAddEditPrices.Text = "ADD AND EDIT PRICES";
+            // 
+            // groupControlCurrentPrices
+            // 
+            this.groupControlCurrentPrices.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupControlCurrentPrices.Location = new System.Drawing.Point(0, 0);
+            this.groupControlCurrentPrices.Name = "groupControlCurrentPrices";
+            this.groupControlCurrentPrices.Size = new System.Drawing.Size(714, 702);
+            this.groupControlCurrentPrices.TabIndex = 2;
+            this.groupControlCurrentPrices.Text = "CURRENT PRICES";
             // 
             // ItemStorePricesView
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1204, 578);
+            this.ClientSize = new System.Drawing.Size(1436, 702);
+            this.Controls.Add(this.splitContainerMain);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "ItemStorePricesView";
             this.Text = "Item Store Prices View";
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainerMain)).EndInit();
+            this.splitContainerMain.ResumeLayout(false);
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.groupControlStore)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.groupControlUnits)).EndInit();
+            this.splitContainer2.Panel1.ResumeLayout(false);
+            this.splitContainer2.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainerPrices)).EndInit();
+            this.splitContainerPrices.ResumeLayout(false);
+            this.splitContainer2.Panel1.ResumeLayout(false);
+            this.splitContainer2.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).EndInit();
+            this.splitContainer2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.groupControlItems)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.groupControlAddEditPrices)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.groupControlCurrentPrices)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainerMain;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private DevExpress.XtraEditors.GroupControl groupControlStore;
+        private DevExpress.XtraEditors.GroupControl groupControlUnits;
+        private System.Windows.Forms.SplitContainer splitContainerPrices;
+        private System.Windows.Forms.SplitContainer splitContainer2;
+        private DevExpress.XtraEditors.GroupControl groupControlItems;
+        private DevExpress.XtraEditors.GroupControl groupControlAddEditPrices;
+        private DevExpress.XtraEditors.GroupControl groupControlCurrentPrices;
     }
 }
