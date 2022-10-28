324f01ebd9554f3593489e9cbfd4fd2cf00b8949
Ibrahim Hussain <ibrahim.hucyn@live.com>
2022-10-28 13:46:15 +0500

    refactoting

--- ShoppingBird.Desktop/Views/ItemStorePricesView.Designer.cs
+++ ShoppingBird.Desktop/Views/ItemStorePricesView.Designer.cs
@@ -36,7 +36,7 @@ namespace ShoppingBird.Desktop.Views
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1413, 703);
+            this.ClientSize = new System.Drawing.Size(1204, 578);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "ItemStorePricesView";
             this.Text = "Item Store Prices View";
