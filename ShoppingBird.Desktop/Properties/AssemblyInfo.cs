264872d77cee5ebdfd3cef0a20e6317e01fee5e1
Ibrahim Hussain <ibrahim.hucyn@live.com>
2022-11-03 13:13:59 +0500

    version changed as per ammendment

--- ShoppingBird.Desktop/Properties/AssemblyInfo.cs
+++ ShoppingBird.Desktop/Properties/AssemblyInfo.cs
@@ -28,5 +28,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2022.1102.2219.1")]
-[assembly: AssemblyFileVersion("2022.1102.2219.1")]
+[assembly: AssemblyVersion("2022.1103.1312.1")]
+[assembly: AssemblyFileVersion("2022.1103.1312.1")]
